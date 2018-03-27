662185c84ea584a8722a302525eb0c9dcd74cbad
awesomehumphrey <humphreyluv@gmail.com>
2018-03-27 12:23:35 +1100

    Adopted Bootstrap-vue for UI components

--- src/docs/HomePage.vue
+++ src/docs/HomePage.vue
@@ -1,11 +1,38 @@
 <template>
-<nav class="navbar fixed-top navbar-expand-lg navbar-dark info-color" position="top">
-        <a class="navbar-brand" href="#">Gravity++</a>
-</nav>
+<div>
+<b-navbar toggleable="md" type="dark" variant="info">
+
+  <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+
+  <b-navbar-brand href="#">Gravity++</b-navbar-brand>
+
+  <b-collapse is-nav id="nav_collapse">
+
+  </b-collapse>
+</b-navbar>
+
+<b-card no-body>
+  <b-tabs card>
+    <b-tab title="DATA" active>
+      <b-row>
+        <b-col col lg="2" class="tabsections" id="datasection1">1 of 4</b-col>
+        <b-col col lg="1" class="tabsections" id="datasection2">2 of 4</b-col>
+        <b-col col lg="6" class="tabsections" id="datasection3">3 of 4</b-col>
+        <b-col col lg="2" class="tabsections" id="datasection4">4 of 4</b-col>
+    </b-row>
+    </b-tab>
+    <b-tab title="STORY">
+      Tab Contents 2
+    </b-tab>
+  </b-tabs>
+</b-card>
+
+</div>
 
 </template>
 
 <script>
+
 export default {
   data() {
     return {
@@ -33,6 +60,27 @@ li {
 
 a {
   color: #42b983;
+}
+
+.tabsections {
+  margin: 5px;
+  min-height: 500px;
+}
+
+#datasection1 {
+  background-color: #42b983;
+}
+
+#datasection2 {
+  background-color: blue;
+}
+
+#datasection3 {
+  background-color: #c4c7c5;
+}
+
+#datasection4 {
+  background-color: #b9428b;
 }
 
 </style>
--- src/main.js
+++ src/main.js
@@ -1,18 +1,25 @@
+/* eslint-disable */
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
 import App from './App';
 import router from './router';
+import Vuetify from 'vuetify';
+
+import BootstrapVue from 'bootstrap-vue';
 
 import 'bootstrap/dist/css/bootstrap.css';
 import 'mdbootstrap/css/mdb.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
+Vue.use(Vuetify);
+Vue.use(BootstrapVue);
 Vue.config.productionTip = false;
 
-/* eslint-disable no-new */
+///* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  router,
-  template: '<App/>',
-  components: { App }
+    el: '#app',
+    router,
+    template: '<App/>',
+    components: { App }
 });
