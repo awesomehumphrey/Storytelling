b558a3e73e637cb11ec0e6608f8a5610c3c6a0ad
awesomehumphrey <humphreyluv@gmail.com>
2018-06-05 18:36:31 +1000

    Cleaned up data communication between Data and Axis components
    Cleaned up data communication between Data and Axis components

--- src/mycomponents/Axis.vue
+++ src/mycomponents/Axis.vue
@@ -1,9 +1,22 @@
 <template>
 <div>
     <hr>
-   <p @click="">Axis {{dataValues}}</p>
-   <hr>
-   <p @click="">Axis {{fieldNames}}</p>
+    <label for="y-axis">Y-Axis</label>
+    <select v-model="ySelected">
+        <option v-for="(option, index) in fieldNames" v-bind:value="fieldNames[index]">
+            {{fieldNames[index]}}
+        </option>
+    </select>
+    <span>Selected: {{ ySelected }}</span>
+    <br>
+    <label for="x-axis">X-Axis</label>
+    <select v-model="xSelected">
+        <option v-for="(option, index) in fieldNames" v-bind:value="fieldNames[index]">
+            {{fieldNames[index]}}
+        </option>
+    </select>
+    <span>Selected: {{ xSelected }}</span>
+
 </div>
 </template>
 
@@ -15,20 +28,23 @@ export default {
     data() {
         return {
          dataValues: 0,
-         fieldNames: ''
+         fieldNames: '',
+         ySelected: '',
+         xSelected: ''
         }
     },
     created() {
-        DataBus.$on('dataJson', (dataJson) => {
+        DataBus.$on('dataJson', (dataJson) => {  //Receive the data (array of data values) from Data component via DataBus
             this.dataValues = dataJson;
             console.log(this.dataValues);
+            this.ySelected = '';
+            this.xSelected = '';
         });
-        DataBus.$on('fieldArray', (fieldArray) => {
+        DataBus.$on('fieldArray', (fieldArray) => { //Receive the data (field names) from Data component via DataBus
             this.fieldNames = fieldArray;
             console.log(this.fieldNames);
         });
     }
-
 }
 
 </script>
--- src/mycomponents/Data.vue
+++ src/mycomponents/Data.vue
@@ -11,15 +11,9 @@
 <script>
 /* eslint-disable */
 import Papa from 'papaparse'
-import dl from 'datalib'
+import dl from 'datalib'    // ToDo - remove datalib and dataforge from Data component and use only in the components they are needed
 import * as dataForge from 'data-forge';
 import { DataBus } from '@/main';
-
-
-var data;
-var csv;
-var baa;
-var df;
 
 export default {
     data() {
@@ -34,12 +28,11 @@ export default {
         },
         onUpload() {
         // upload file, get it from this.selectedFile
-            Papa.parse(this.selectedFile, {
+            Papa.parse(this.selectedFile, { //parse blob into array of Json(if header is true) or array of rows (if header is false)
                 header: true,
                 dynamicTyping: true,
                 complete: function(results) {
-                data = results;   // results is an array of four objects: data, errors, meta (fields is contained within meta)
-                
+                //data = results;   // results is an array of three objects: data, errors, meta (fields is contained within meta)
                 DataBus.$emit('dataJson', results.data); // Send the data values through the event bus
                 DataBus.$emit('fieldArray', results.meta.fields);// Send the field names through the event
                 }
