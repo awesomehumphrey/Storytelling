93e9cde9df2d46ba1d7de2d01cc2278c92350a32
awesomehumphrey <humphreyluv@gmail.com>
2019-01-24 19:28:09 +1100

    Charts in storytab are now responsive
    Charts in storytab are now response. The charts in reveal slides are no
    longer blurry.

--- src/mycomponents/Story.vue
+++ src/mycomponents/Story.vue
@@ -1,24 +1,26 @@
 <template>
-  <b-row>
-    <b-col col lg="2" id="storyOps"></b-col>
-    <b-col col lg="9.5" class="storySections">
-      <b-row>
-        <b-col col lg="9.5">
-          <div id="pres">
-            <div class="reveal">
-              <div class="slides">
-                <section>Gravity++: Telling stories with data...</section>
-                <section :id="item" v-for="(item, index) in id" :key="index">{{item}}</section>
-                <section>Thank you!</section>
+  <div>
+    <b-row>
+      <b-col col lg="2" id="storyOps"></b-col>
+      <b-col col lg="9.5" class="storySections">
+        <b-row>
+          <b-col col lg="9.5">
+            <div id="pres" ref="bar" v-on:resize="handleResize()">
+              <div class="reveal">
+                <div class="slides">
+                  <section>Gravity++: Telling stories with data...</section>
+                  <section :id="item" v-for="(item, index) in id" :key="index">{{item}}</section>
+                  <section>Thank you!</section>
+                </div>
+                <!--Move 'Thank you' to a different div before adding conditional statements or render it dynamically like the charts-->
               </div>
-              <!--Move 'Thank you' to a different div before adding conditional statements-->
             </div>
-          </div>
-        </b-col>
-        <b-col col lg="2" id="presOps"></b-col>
-      </b-row>
-    </b-col>
-  </b-row>
+          </b-col>
+          <b-col col lg="2" id="presOps"></b-col>
+        </b-row>
+      </b-col>
+    </b-row>
+  </div>
 </template>
 
 <script>
@@ -27,6 +29,7 @@ import { DataBus } from "@/main";
 import Reveal from "reveal.js/js/reveal";
 import vegaEmbed from "vega-embed";
 var myNodes = []; //To hold non-reactive array of objects(nodes) because reactive data doesn't work nicely with v-for directive in the template
+
 export default {
   data() {
     return {
@@ -37,16 +40,25 @@ export default {
     DataBus.$on("nodeArray", this.getNodeArray); //receive connection/sequenced ordered node array from graph canvas and handle in a dedicated function
   },
   mounted() {
-    Reveal.initialize({ transition: "zoom", embedded: true });
+    Reveal.initialize({
+      transition: "zoom",
+      embedded: true,
+      margin: 0,
+      minScale: 1,
+      maxScale: 1
+    });
+    this.$nextTick(() => {
+      window.addEventListener("resize", this.handleResize);
+      window.addEventListener("load", this.handleLoad);
+    });
   },
   updated() {
-    for (var i = 0; i < myNodes.length; i++) {
-      myNodes[i].nData.title = myNodes[i].label; //Add graph title to graph spec
-      vegaEmbed("#" + this.id[i], myNodes[i].nData, {
-        defaultStyle: false,
-        actions: false
-      });
-    }
+    //this.renderVisStory();
+    this.handleLoad();
+  },
+  beforeDestroy() {
+    window.removeEventListener("resize", this.handleResize);
+    window.addEventListener("load", this.handleLoad);
   },
   methods: {
     getNodeArray(nodeArray) {
@@ -64,6 +76,33 @@ export default {
         //console.log(this.id[i]);
         //console.log(myNodes[i].nData);
       }
+    },
+    renderVisStory() {
+      for (var i = 0; i < myNodes.length; i++) {
+        myNodes[i].nData.title = myNodes[i].label; //Add graph title to graph spec
+        vegaEmbed("#" + this.id[i], myNodes[i].nData, {
+          defaultStyle: false,
+          actions: false
+        });
+      }
+    },
+    handleResize(event) {
+      //console.log(this.$refs.bar.clientWidth);
+      for (var i = 0; i < myNodes.length; i++) {
+        if (this.$refs.bar.clientWidth >= 1000) {
+          myNodes[i].nData.width = this.$refs.bar.clientWidth - 360;
+        } else {
+          myNodes[i].nData.width = this.$refs.bar.clientWidth - 160;
+        }
+      }
+      this.renderVisStory();
+    },
+    handleLoad(event) {
+      for (var i = 0; i < myNodes.length; i++) {
+        myNodes[i].nData.width = document.documentElement.clientWidth / 2 + 40;
+      }
+      this.renderVisStory();
+      //console.log(event.currentTarget.innerWidth / 2 + 40);
     }
   }
 };
@@ -103,7 +142,7 @@ export default {
   border: 1px solid grey; /* border: 1px solid grey; */
   border-radius: 2px;
   background-color: #f1f1f1;
-  max-width: 250px;
+  /* max-width: 300px; */
 }
 .storySections {
   margin: 5px;
